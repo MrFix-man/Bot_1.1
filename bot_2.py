07e87d5fc4f8ce6bb04a8fb28a8858c879eee00a
David Dik <49565672+MrFix-man@users.noreply.github.com>
2023-11-03 11:11:02 +0100

    dfhgs

--- bot_2.py
+++ bot_2.py
@@ -5,9 +5,9 @@ import sourse
 bot_client = telebot.TeleBot(token=sourse.token)
 
 
-@bot_client.message_handler(commands=['старт', 'start'])
+@bot_client.message_handler(commands=['старт', 'start', 'привет', 'Привет'])
 def echo(massage: Message):
-    bot_client.send_message(chat_id=massage.chat.id, text='Send')
+    bot_client.send_message(chat_id=massage.chat.id, text='Я люблю тебя:)')
     # print(massage.chat.id)
     c = 1
 
